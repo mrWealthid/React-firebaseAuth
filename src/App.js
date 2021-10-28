883107f8d4affeb80f467b8ede22d53ed9ed1af8
Wealth Iduwe <wealthiduwe@gmail.com>
2021-10-28 18:10:25 +0100

    debugging

--- src/App.js
+++ src/App.js
@@ -7,29 +7,30 @@ import Profile from './components/Profile';
 import PrivateRoutes from './components/PrivateRoutes';
 
 import DashBoard from './components/DashBoard';
-// import { useAuthContext } from './AuthContext';
+import { useAuthContext } from './AuthContext';
 import UpdateProfile from './components/UpdateProfile';
 import ResetPassword from './components/ResetPassword';
 import Home from './components/Home';
 
 function App() {
-  // const { users } = useAuthContext();
+  const { users } = useAuthContext();
   return (
     <div className='flex flex-col w-10/12 mx-auto justify-center'>
       <Navlinks />
 
       <section className='min-h-screen gap-4 flex flex-col justify-center items-center '>
-        {/* {users && (
+        {users && (
           <h5 className='text-black-600 text-lg'>
             User Logged In: {users?.email}
           </h5>
-        )} */}
+        )}
 
         <Switch>
           <Route exact path={'/'} component={Home} />
-          <PrivateRoutes exact path='/Profile' component={Profile} />
+          <PrivateRoutes path='/Profile' component={Profile} />
           <PrivateRoutes path='/Dashboard' component={DashBoard} />
           <Route path={'/Signup'} component={Signup} />
+
           <Route path={'/login'} component={Login} />
           <Route path={'/update-profile'} component={UpdateProfile} />
           <Route path={'/resetPassword'} component={ResetPassword} />
